a935ad1c171506f5628704be330e208cc71ab561
Ranjeet Singh Tharu <67905362+Ranjeetprgrmr@users.noreply.github.com>
2025-08-03 18:40:01 +0545

    guide to pushing code to github

--- commands.py
+++ commands.py
@@ -19,3 +19,36 @@
 # register user
 # username = manager1
 # password = password123!@#
+
+
+
+
+# pushing the code to github
+
+# PS E:\PYTHON\2pythonCrudAppDeployed-CWD> git init
+# Reinitialized existing Git repository in E:/PYTHON/2pythonCrudAppDeployed-CWD/.git/
+# PS E:\PYTHON\2pythonCrudAppDeployed-CWD> git add .
+# PS E:\PYTHON\2pythonCrudAppDeployed-CWD> git commit -m "initial commit"
+# On branch main
+# nothing to commit, working tree clean
+# PS E:\PYTHON\2pythonCrudAppDeployed-CWD> git remote add origin https://github.com/Ranjeetprgrmr/2pythoncrudapp.git
+# PS E:\PYTHON\2pythonCrudAppDeployed-CWD> git remote -v
+# origin  https://github.com/Ranjeetprgrmr/2pythoncrudapp.git (fetch)
+# origin  https://github.com/Ranjeetprgrmr/2pythoncrudapp.git (push)
+# PS E:\PYTHON\2pythonCrudAppDeployed-CWD> git push origin master
+# error: src refspec master does not match any
+# error: failed to push some refs to 'https://github.com/Ranjeetprgrmr/2pythoncrudapp.git'
+# PS E:\PYTHON\2pythonCrudAppDeployed-CWD> git remote add origin https://github.com/Ranjeetprgrmr/2pythoncrudapp.git
+# error: remote origin already exists.
+# PS E:\PYTHON\2pythonCrudAppDeployed-CWD> git push -u origin main
+# Enumerating objects: 56, done.
+# Counting objects: 100% (56/56), done.
+# Delta compression using up to 12 threads
+# Compressing objects: 100% (52/52), done.
+# Writing objects: 100% (56/56), 26.02 KiB | 783.00 KiB/s, done.
+# Total 56 (delta 6), reused 0 (delta 0), pack-reused 0 (from 0)
+# remote: Resolving deltas: 100% (6/6), done.
+# To https://github.com/Ranjeetprgrmr/2pythoncrudapp.git
+#  * [new branch]      main -> main
+# branch 'main' set up to track 'origin/main'.
+# PS E:\PYTHON\2pythonCrudAppDeployed-CWD> 
